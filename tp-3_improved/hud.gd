7005fc2d3ba8161f7524b39822ed7ed214087756
TTylerRRichard <2496174@cmontmorency.qc.ca>
2025-12-11 22:12:40 -0500

    workingon-enemy

--- tp-3_improved/hud.gd
+++ tp-3_improved/hud.gd
@@ -1,10 +1,12 @@
 extends CanvasLayer
 
 @onready var coin_label = $CoinLabel
-
+@onready var goal_label = $GoalLabel
 func _ready():
 	add_to_group("hud")
 	update_coin_label(global.coins)
+
+
 
 func update_coin_label(value):
 	coin_label.text = str(value)
